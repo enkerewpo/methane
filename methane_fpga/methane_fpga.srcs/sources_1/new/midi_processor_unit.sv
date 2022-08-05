d0231f47123c32d099bd738132beb77a2010497a
wheatfox <enkerewpo@hotmail.com>
2022-08-05 12:11:15 +0800

    midi

---
+++ methane_fpga/methane_fpga.srcs/sources_1/new/midi_processor_unit.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: wheatfox
+// Engineer: wheatfox
+// 
+// Create Date: 08/05/2022 11:59:24 AM
+// Design Name: midi processor
+// Module Name: midi_processor_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module midi_processor_unit
+(
+    input clk,
+    input rst
+);
+
+    
+
+endmodule
