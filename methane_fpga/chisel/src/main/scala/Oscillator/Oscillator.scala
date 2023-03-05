b680ec81df3a145912b52a1e1ac2ce39c19bd370
wheatfox <enkerewpo@hotmail.com>
2023-03-05 22:48:10 +0800

    implemented simple freq change

--- methane_fpga/chisel/src/main/scala/Oscillator/Oscillator.scala
+++ methane_fpga/chisel/src/main/scala/Oscillator/Oscillator.scala
@@ -2,6 +2,8 @@ package Oscillator
 
 import chisel3._
 import chisel3.util._
+import chisel3.experimental.FixedPoint
+
 
 /**
  * Oscillator module
@@ -18,19 +20,32 @@ class Oscillator()
         val out = Output(SInt(16.W)) // output 16bit sample point (signed)
     })
 
-    val waveframe = VecInit(
+    val waveform = VecInit(
         Seq.tabulate(256)(
-            i => (scala.math.sin(2 * scala.math.Pi * i / 256) * 20000).toInt.S(16.W)
+            i => (
+                scala.math.sin(2 * scala.math.Pi * i / 256)
+                * 20000).toInt.S(16.W)
         )
     )
-    // now test with a simple sine wave frame
+    // now test with a simple sine waveform
 
     val samplePoint = Wire(SInt(16.W))
-    val testReg = RegInit(0.U(16.W))
+    val holdTick = RegInit(78125.U(32.W) / (io.freq * 2.U(32.W)))
+    val runTick = RegInit(0.U(32.W))
+    val idx = RegInit(0.U(16.W))
+
     when(io.en) {
-        testReg := testReg + 1.U(16.W)
+        holdTick := 78125.U(32.W) / (io.freq * 2.U(32.W))
+        runTick := runTick + 1.U(32.W)
+        when(runTick === holdTick) {
+            runTick := 0.U(32.W)
+            idx := idx + 1.U(16.W)
+            when(idx === 256.U(16.W)) {
+                idx := 0.U(16.W)
+            }
+        }
     }
-    samplePoint := waveframe(testReg)
+    samplePoint := waveform(idx)
     io.out := samplePoint
 }
 
@@ -39,3 +54,10 @@ object OscillatorGen extends App {
         Array("--target-dir", "output/")
     )
 }
+
+/**
+ * 10M -> 100ns
+ * freq -> 1/freq
+ * idx -> (1/freq)/256/100, one idx remain tick count
+ *          78125/(freq * 2)
+ */
